9104d39ff2eed4695bca6eaece9466f088d3e854
gnubyte <phastings@openmobo.com>
2018-08-01 08:43:08 -0400

    Update and rename singleAioInstance.sh to RHELsingleAioInstance.sh
    reordered to fix commands, and fixed runtime user

--- RHELsingleAioInstance.sh
+++ RHELsingleAioInstance.sh
@@ -1,5 +1,7 @@
-# @Date: 02-25-2018
+# @Date: 08-01-2018
+# @Version: 1.1
 # @Author: Patrick Hastings
+# @Distro: CentOS 7.x x86_64
 # @Instructions: Copy and paste this into a server and run with elevated permissions
 # @Note: this will install splunk to opt directory by default
 # ------
@@ -10,11 +12,11 @@ sudo yum install nano -y && sudo yum update -y && sudo yum install wget -y
 sudo yum install zip -y && sudo yum install unzip -y
 wget -O splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.1&product=splunk&filename=splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz&wget=true'
 sudo tar xvzf splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz -C /opt
-export SPLUNK_HOME=/opt/splunk/ >> ~/.bashrc
-. ~/.bashrc
 sudo groupadd splunk
 sudo adduser splunk -g splunk
 sudo chown -R splunk:splunk /opt/splunk
 sudo -H -u splunk /opt/splunk/bin/splunk start --accept-license
 sudo -H -u splunk /opt/splunk/bin/splunk edit user admin -password changed -role admin -auth admin:changeme 
-sudo -H -u splunk /opt/splunk/bin/splunk enable boot-start -user splunk
+sudo -H /opt/splunk/bin/splunk enable boot-start -user splunk
+export SPLUNK_HOME=/opt/splunk/ >> /home/splunk/.bashrc
+. /home/splunk/.bashrc
