9692b1c200ceebbf0f9ffd2735f144a42cae6b14
gnubyte <phastings@openmobo.com>
2021-11-01 15:06:16 -0700

    Update DebianAioInstance.sh
    Updated Splunk to splunk-8.2.3

--- DebianAioInstance.sh
+++ DebianAioInstance.sh
@@ -14,8 +14,10 @@ sudo apt-get install nano -y && sudo apt-get update -y && sudo apt-get install w
 sudo apt-get install zip -y && sudo apt-get install unzip -y
 sudo apt-get install fail2ban -y
 sudo apt-get install ufw -y
-wget -O splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.1&product=splunk&filename=splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz&wget=true'
-sudo tar xvzf splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz -C /opt
+#wget -O splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.1&product=splunk&filename=splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz&wget=true'
+#wget -O splunk-8.2.3-cd0848707637-linux-2.6-amd64.deb 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-linux-2.6-amd64.deb'
+wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz'
+sudo tar xvzf splunk-8.2.3-cd0848707637-Linux-x86_64.tgz -C /opt
 sudo groupadd splunk
 sudo adduser --ingroup splunk --disabled-password --gecos "" splunk
 sudo chown -R splunk:splunk /opt/splunk
