da3e53812036cfd30abc7a594b9aae1b6e513332
gnubyte <phastings@openmobo.com>
2023-02-23 10:11:35 -0800

    Update DebianAioInstance.sh

--- DebianAioInstance.sh
+++ DebianAioInstance.sh
@@ -16,8 +16,10 @@ sudo apt-get install fail2ban -y
 sudo apt-get install ufw -y
 #wget -O splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz 'https://www.splunk.com/bin/splunk/DownloadActivityServlet?architecture=x86_64&platform=linux&version=7.0.1&product=splunk&filename=splunk-7.0.1-2b5b15c4ee89-Linux-x86_64.tgz&wget=true'
 #wget -O splunk-8.2.3-cd0848707637-linux-2.6-amd64.deb 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-linux-2.6-amd64.deb'
-wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz'
-sudo tar xvzf splunk-8.2.3-cd0848707637-Linux-x86_64.tgz -C /opt
+#wget -O splunk-8.2.3-cd0848707637-Linux-x86_64.tgz 'https://download.splunk.com/products/splunk/releases/8.2.3/linux/splunk-8.2.3-cd0848707637-Linux-x86_64.tgz'
+wget -O splunk-9.0.4-de405f4a7979-linux-2.6-amd64.deb "https://download.splunk.com/products/splunk/releases/9.0.4/linux/splunk-9.0.4-de405f4a7979-linux-2.6-amd64.deb"
+#sudo tar xvzf splunk-8.2.3-cd0848707637-Linux-x86_64.tgz -C /opt
+sudo dpkg -i splunk-9.0.4-de405f4a7979-linux-2.6-amd64.deb
 sudo groupadd splunk
 sudo adduser --ingroup splunk --disabled-password --gecos "" splunk
 sudo chown -R splunk:splunk /opt/splunk
